9ccdc1cfaa4006b7c814fb7957b05cbb14c92a5b
Terry Rice <terryrice@pop-os.attlocal.net>
2025-06-30 17:27:34 -0500

    Added cmake and updates stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -197,6 +197,7 @@ return  {
       clangd = {},
       gopls = {},
       pyright = {},
+      cmake = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
