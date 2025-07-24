8016876e486a07b2d8beec74b253a11de3d4feb5
Terry Rice <trice7454@gmail.com>
2025-07-23 19:30:49 -0500

    Added a process attach for debugging already started processes

--- init.lua
+++ init.lua
@@ -102,6 +102,12 @@ require('dap').configurations.cs = {
         return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
     end,
   },
+  {
+    type = "coreclr",
+    name = "attach - netcoredbg",
+    request = "attach",
+    processId = require('dap.utils').pick_process,
+  },
 }
 
 
